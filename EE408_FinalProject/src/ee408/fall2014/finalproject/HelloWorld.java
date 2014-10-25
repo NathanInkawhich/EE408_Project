c0b65bc4008078a2ca89ff22c61807463cfa383d
Nate <ninkawhich55@gmail.com>
2014-10-24 23:31:10 -0400

    Testing Git repository across two computers

--- EE408_FinalProject/src/ee408/fall2014/finalproject/HelloWorld.java
+++ EE408_FinalProject/src/ee408/fall2014/finalproject/HelloWorld.java
@@ -3,5 +3,6 @@ package ee408.fall2014.finalproject;
 public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello World!");
+		System.out.println("This was modified in my Mac");
 	}
 }
