1d8c4916c8c0c69508466d6bc3a0dd5aac67c76e
Nathan A Inkawhich <inkawhna@clarkson.edu>
2014-11-09 19:23:47 -0500

    This commit contains the structure of our database.  I created a new JobPosting class which will represent the jobs in our database.

---
+++ EE408_FinalProject/src/ee408/fall2014/finalproject/JobPosting.java
@@ -0,0 +1,91 @@
+package ee408.fall2014.finalproject;
+
+import java.util.*;
+
+public class JobPosting {
+	
+	private String companyName;
+	private String jobDescription;
+	private boolean fullTime;
+	private String jobType;//Health care, engineering, etc...
+	private Set<String> keywords;
+	
+	public JobPosting(String companyName, String jobDescription, boolean fullTime, String jobType, Set<String> keys){
+		
+		this.companyName = companyName;
+		this.jobDescription = jobDescription;
+		this.fullTime = fullTime;
+		this.jobType = jobType;
+		keywords = new HashSet<String>(keys);
+	
+	}
+	public void addKeywords(Set<String> keys){
+		this.keywords = keys;
+	}
+	public HashSet<String> getKeywords(){
+		return (HashSet<String>) keywords;
+	}
+	public String getCompanyName(){
+		return this.companyName;
+	}
+	public String getJobDescription(){
+		return this.jobDescription;
+	}
+	public boolean getFullTime(){
+		return this.fullTime;
+	}
+	public String getJobType(){
+		return this.jobType;
+	}
+	
+	public static void main(String[] args){
+		
+		ArrayList<JobPosting> jobsList = new ArrayList<JobPosting>();
+		String [] companyNames = {"IBM","Birnie Bus","Emerson's Landscaping"};
+		String [] jobDescriptions = {"We work on computers","We give people rides","We cut grass"};
+		boolean [] jobTimes = {true,true,false};
+		String [] jobTypes = {"Engineering", "Business", "Manual Labor"};
+		ArrayList<Set<String>> keywordList = new ArrayList<Set<String>>();
+		
+		HashSet<String> IBMKeys = new HashSet<String>();
+		IBMKeys.add("computers");
+		IBMKeys.add("mainframes");
+		IBMKeys.add("coffee");
+		HashSet<String> BirnieKeys = new HashSet<String>();
+		BirnieKeys.add("busses");
+		BirnieKeys.add("transportation");
+		BirnieKeys.add("travel");
+		HashSet<String> EmersonKeys = new HashSet<String>();
+		EmersonKeys.add("trees");
+		EmersonKeys.add("grass");
+		EmersonKeys.add("lawnmowers");
+		
+		keywordList.add(IBMKeys);
+		keywordList.add(BirnieKeys);
+		keywordList.add(EmersonKeys);
+		
+		for(int i = 0; i<companyNames.length; i++){
+			JobPosting jp = new JobPosting(companyNames[i],jobDescriptions[i], jobTimes[i], jobTypes[i], keywordList.get(i));
+			jobsList.add(jp);
+		}
+		
+		for(int j = 0; j<jobsList.size();j++){
+			JobPosting temp = jobsList.get(j);
+			System.out.println("**********");
+			System.out.println(temp.getCompanyName());
+			System.out.println(temp.getJobDescription());
+			System.out.println(temp.getFullTime());
+			System.out.println(temp.getJobType());
+			System.out.print("Company Keywords: ");
+			HashSet<String> tempKeys = temp.getKeywords();
+			Iterator<String> itr = tempKeys.iterator();
+			while(itr.hasNext()){
+				System.out.print(itr.next()+" ");
+			}
+			System.out.println();
+		}
+	
+	}
+	
+	
+}
